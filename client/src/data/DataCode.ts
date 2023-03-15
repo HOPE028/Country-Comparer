e923930fca227d75627ea53e0f0b60e0b26866e3
Pasha Khoshkebari <pkhoshkeba@smail.pembinatrails.ca>
2023-03-15 12:07:01 -0500

    Added data signatures from WB

---
+++ client/src/data/DataCode.ts
@@ -0,0 +1,11 @@
+interface interfaceDataCodes {
+  [key: string]: string
+}
+
+export const DataCodes: interfaceDataCodes = {
+  'SP.POP.TOTL': 'Population total',
+  'SP.POP.GROW': 'Population growth (annual %)',
+  'SP.DYN.LE00.IN': 'Life expectancy at birth, total (years)',
+  'SL.UEM.TOTL.ZS':
+    'Unemployment, total (% of total labor force) (modeled ILO estimate)',
+}
---
+++ client/src/data/data.ts
@@ -0,0 +1,8 @@
+export const data = [
+  'SP.POP.TOTL',
+  'SP.POP.GROW',
+  'SP.DYN.LE00.IN',
+  'SL.UEM.TOTL.ZS',
+  'NY.GDP.MKTP.CD',
+  'NY.GDP.PCAP.CD',
+]

9cbccb51321cc516865be43dbd8a463315857922
Pasha Khoshkebari <pkhoshkeba@smail.pembinatrails.ca>
2023-03-16 11:59:22 -0500

    Filtered data from api to only show important information. Current problem: Label can not be fetched

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import ChoosingCountries from './components/ChoosingCountries'
+import { DataCodes } from './data/DataCode'
+import { data } from './data/data'
 // import Test from './test'
 import './App.css'
 
@@ -24,34 +26,71 @@ function App() {
   const [countryOneData, setCountryOneData] = useState()
   const [countryTwoData, setCountryTwoData] = useState()
 
+  // Filterad Data to be shown.
+  interface dataFiltered {
+    dataLabel: string
+    countryOneData: string | number
+    countryTwoData: string | number
+  }
+
+  const [dataFiltered, setDataFiltered] = useState<Array<dataFiltered>>([])
+
   const fetchData = (countryOneCode: string, countryTwoCode: string) => {
     // Set flags to the correct country.
     setCountryOneImage(`https://countryflagsapi.com/png/${countryOneCode}`)
     setCountryTwoImage(`https://countryflagsapi.com/png/${countryTwoCode}`)
 
     // Creating urls for country data
-    const WBDataURL = `https://data.nasdaq.com/api/v3/datatables/WB/DATA?series_id=VC.PKP.TOTL.UN,SP.POP.TOTL&country_code=${countryOneCode},${countryTwoCode}&api_key=wCaQ4RwKqax92nzyh_px`
+    const WBDataURL = `https://data.nasdaq.com/api/v3/datatables/WB/DATA?series_id=${data.map(
+      (field) => `${field},`
+    )}&country_code=${countryOneCode},${countryTwoCode}&api_key=wCaQ4RwKqax92nzyh_px`
 
     axios
       .get(WBDataURL)
       .then((res) => res.data)
       .then((res) => res.datatable)
       .then((res) => res.data)
-      .then((res) => sortData(res))
+      .then((res) => sortData(res, countryOneCode, countryTwoCode))
       .catch((error) => console.log(error))
   }
 
   interface countryData {
-    series_id: string
-    country_code: string
-    country_name: string
-    year: number
-    value: number
+    // series_id: string
+    // country_code: string
+    // country_name: string
+    // year: number
+    // value: number
+    [index: number]: string | number
   }
 
-  const sortData = (data: Array<countryData>) => {
-    console.log(data)
-    for (const field in data) {
+  const sortData = (
+    dataFromWB: Array<countryData>,
+    countryOneCode: string,
+    countryTwoCode: string
+  ) => {
+    console.log(dataFromWB)
+
+    for (const code of data) {
+      const list = dataFromWB.filter((field) => field[0] === code)
+
+      const listCountryOne = list.filter((field) => field[1] === countryOneCode)
+
+      const listCountryTwo = list.filter((field) => field[1] === countryTwoCode)
+
+      const label = DataCodes[code]
+
+      console.log(label)
+
+      const temporaryFilteredObject: dataFiltered = {
+        dataLabel: label,
+        countryOneData: listCountryOne[0][4],
+        countryTwoData: listCountryTwo[0][4],
+      }
+
+      let temporaryWholeDataFilteredObject = dataFiltered
+      temporaryWholeDataFilteredObject.push(temporaryFilteredObject)
+
+      setDataFiltered(temporaryWholeDataFilteredObject)
     }
   }
 
@@ -74,18 +113,20 @@ function App() {
         fetchData={fetchData}
       />
 
-      <img
+      {/* <img
         crossOrigin='anonymous'
         src={countryImage}
         style={{ width: '300px' }}
-      />
+      /> */}
 
-      <img
+      {/* <img
         crossOrigin='anonymous'
         src='https://countryflagsapi.com/png/ae'
         alt='United Arab Emirates flag'
         style={{ width: '300px' }}
-      />
+      /> */}
+
+      <button onClick={() => console.log(dataFiltered)}>DATA</button>
     </div>
   )
 }
--- client/src/data/DataCode.ts
+++ client/src/data/DataCode.ts
@@ -8,4 +8,6 @@ export const DataCodes: interfaceDataCodes = {
   'SP.DYN.LE00.IN': 'Life expectancy at birth, total (years)',
   'SL.UEM.TOTL.ZS':
     'Unemployment, total (% of total labor force) (modeled ILO estimate)',
+  'NY.GDP.MKTP.CD': 'GDP (current US$)',
+  'NY.GDP.PCAP.CD': 'GDP per capita (current US$)',
 }
